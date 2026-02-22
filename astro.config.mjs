cc075a1995e1bbd8f462f78663f461e9bc7359a9
kiitoss <antoinechataignier@gmail.com>
2026-02-22 21:14:44 +0100

    cd: gh-page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kiitoss.github.io",
+  base: '/website-eva',
+
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
 
   integrations: [react()],
 });
